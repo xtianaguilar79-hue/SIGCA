0b983bd41368047ed42a721c4276b5be3d11501b
xtianaguilar79-hue <xtianaguilar79@gmail.com>
2026-08-13 09:46:19 -0300

    Delete client.ts

--- client.ts
+++
@@ -1,7 +0,0 @@
-import { createBrowserClient } from "@supabase/ssr";
-import { getSupabaseConfig } from "./config";
-
-export function createClient() {
-  const { url, key } = getSupabaseConfig();
-  return createBrowserClient(url, key);
-}
